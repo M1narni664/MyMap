83be1ec662f05bc0df01f6eb17f303b5e0a9ac7c
M1narni664 <minarnibuhana@gmail.com>
2024-09-17 10:39:55 +0700

    marker and basemap dropdown

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,67 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import Map from '@arcgis/core/Map';
 import MapView from '@arcgis/core/views/MapView';
-import { Geolocation } from '@capacitor/geolocation';
 import Graphic from '@arcgis/core/Graphic';
 import Point from '@arcgis/core/geometry/Point';
 import SimpleMarkerSymbol from '@arcgis/core/symbols/SimpleMarkerSymbol';
 import ImageryLayer from '@arcgis/core/layers/ImageryLayer'
-
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-
-export class HomePage {
-  mapView!: MapView | any;
+export class HomePage implements OnInit {
+  mapView!: MapView;
+  map!: Map;
   userLocationGraphic: Graphic | any;
 
   constructor() { }
 
   async ngOnInit() {
-    const map = new Map({
-      basemap: "topo-vector"
+    this.map = new Map({
+      basemap: "topo-vector"  // Default basemap
     });
 
     this.mapView = new MapView({
       container: "container",
-      map: map,
-      zoom: 8
+      map: this.map,
+      zoom: 5,
+      center: [-100.93962439742758, 40.36207168925529] // Center the map on the desired location
     });
 
     let weatherServiceFL = new ImageryLayer({ url: WeatherServiceUrl });
-    map.add(weatherServiceFL);
+    this.map.add(weatherServiceFL);
 
-    await this.updateUserLocationOnMap();
-    this.mapView.center = this.userLocationGraphic.geometry as Point;
-    setInterval(this.updateUserLocationOnMap.bind(this), 10000);
-  }
-  async getLocationService(): Promise<number[]> {
-    return new Promise((resolve, reject) => {
-      navigator.geolocation.getCurrentPosition((resp) => {
-        resolve([resp.coords.latitude, resp.coords.longitude]);
-      })
-    })
+    this.addMarkerAtCoordinates(39.48414730017551, -99.62792052624937);
   }
 
-  async updateUserLocationOnMap() {
-    let latLng = await this.getLocationService();
-    let geom = new Point({ latitude: latLng[0], longitude: latLng[1] });
-    if (this.userLocationGraphic) {
-      this.userLocationGraphic.geometry = geom;
-    } else {
-      this.userLocationGraphic = new Graphic({
-        symbol: new SimpleMarkerSymbol(),
-        geometry: geom,
-      });
-    }
-    this.mapView.graphics.add(this.userLocationGraphic);
+  addMarkerAtCoordinates(lat: number, lon: number) {
+    const point = new Point({
+      latitude: lat,
+      longitude: lon
+    });
+
+    const markerSymbol = new SimpleMarkerSymbol({
+      style: 'diamond',
+      color: 'yellow',
+      outline: {
+        color: 'green',
+        width: 2
+      },
+      size: "30px"  // Set marker size
+    });
+
+    const pointGraphic = new Graphic({
+      geometry: point,
+      symbol: markerSymbol
+    });
+
+    this.mapView.graphics.add(pointGraphic);
+  }
+
+  onBasemapChange(event: any) {
+    const selectedBasemap = event.detail.value;
+    this.map.basemap = selectedBasemap;
   }
 }
 
-const WeatherServiceUrl = 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer'
+const WeatherServiceUrl = 'https://mapservices.weather.noaa.gov/eventdriven/rest/services/radar/radar_base_reflectivity_time/ImageServer';
 
 //   private latitude: number | any;
 //   private longitude: number | any;
